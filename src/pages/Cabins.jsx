9305d07e09e3386049b2bc3559d8199ed5e14d4d
Jawid-7ce <jawid.saraj7ce@gmail.com>
2025-02-25 11:41:19 +0430

    set up react hook form and used it to get form data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,20 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 
 function Cabins() {
-
+const [showForm, setShowForm] = useState(false);
   return (
     <>
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
     </Row>
-    <Row>
+    <Row type="vertical">
       <CabinTable/>
+      <Button size="medium" variation="primary" onClick={() => setShowForm(show => !show)}>Add new cabin</Button>
+      {showForm && <CreateCabinForm/>}
     </Row>
     </>
   );
