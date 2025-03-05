eeab48cb967d4aeaa4ecfa16661fbaab4ba5f7f6
Jawid-7ce <jawid.saraj7ce@gmail.com>
2025-03-05 11:33:46 +0430

    Refractored

---
+++ src/features/cabins/useCreateCabin.js
@@ -0,0 +1,18 @@
+import { useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { createEditCabin } from "../../services/apiCabins";
+import CreateCabinForm from "./CreateCabinForm";
+
+export function useCreateCabin() {
+  const queryClient = useQueryClient();
+  const { mutate: createCabin, isLoading: isCreating } = useMutation({
+    mutationFn: createEditCabin,
+    onSuccess: () => {
+      toast("new cabin added successfully!");
+      queryClient.invalidateQueries({ queryKey: ["cabins"] });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { isCreating, createCabin };
+}
